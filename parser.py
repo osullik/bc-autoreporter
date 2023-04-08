41435eb36c45e94ab43e7f6898cf7e0642cebf6f
Kent O'Sullivan <osullik@protonmail.com>
2023-04-08 12:23:13 -0400

    DateParsing implemented

--- parser.py
+++ parser.py
@@ -3,10 +3,14 @@ import os
 import nltk
 from nltk.tree import Tree
 from nltk.metrics.distance  import edit_distance
+import datefinder, datetime
+import re
 
+'''
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
+'''
 
 # Input Args:
 #	reporter	-	string	-	The person reporting the observation
@@ -75,3 +79,94 @@ class logEntryParser():
 
 
 		return targetEntity
+
+	def parseDates(self, logEntry, defaultYear):	
+		'''
+		Function to extract dates from an employee observation log entry 
+		Assumptions:
+			1. All input dates are in US Format (MM/DD) when that is unclear
+			2. The first date mentioned in the text is the correct date. 
+		Known Bugs: 
+			1. dateFinder will render 34 Feb 2021 as 21 Feb 2034
+			2. dateFinder does not consistently apply MM/DD/YYYY format (day month confusion)
+		InputArgs: 
+			logEntry - String - The text of the log entry 
+			defaultYear - String - the year that will be used if no year is given 
+		Actions: 
+			Parse for dates
+			Format in Elastic-compatible format
+			Return first date mentioned in log
+		Returns: 
+			dateToReturn - String - YYYY/MM/DD format
+		'''	
+
+		print(logEntry)
+		dateToReturn = None
+
+		months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", 
+					"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+		defYear = datetime.datetime.strptime(defaultYear, "%Y")			# Define the default year to use (convert to date)	
+
+		matches = list(datefinder.find_dates(logEntry, 					# Extract list of possible dates
+											base_date=defYear))
+		
+		print(matches)
+		formattedMatches = []											# List to hold the string formatted dates
+
+		if len(matches) > 0:											# Convert to YYYY/MM/DD formatted string
+		    for date in matches:
+		        formattedMatches.append(date.strftime('%Y/%m/%d'))
+
+		    dateToReturn = formattedMatches[0]							# Return the first date mentioned in the text
+		
+		else: 
+			tokens = nltk.word_tokenize(logEntry)						# Split string into tokens
+			monthIndex = None
+			candidateDate = "" 											
+
+			for month in months: 										# Search all string for months 
+				if monthIndex == None: 
+					for token in tokens: 
+						if token in months:
+							monthIndex = tokens.index(token)
+							candidateDate+=(token)
+							break
+				else:
+					break
+
+				strippedToken = tokens[monthIndex+1].strip("st") 	#Anchor on the month, check to see if day or year is next
+				strippedToken = strippedToken.strip("nd")
+				strippedToken = strippedToken.strip("rd")
+				strippedToken = strippedToken.strip("th")
+				if (strippedToken).isnumeric():
+					candidateDate+=(" "+tokens[monthIndex+1])
+
+				strippedNextToken = tokens[monthIndex+2].strip("st") #Anchor on the month, check to see if day or year is next
+				strippedNextToken = strippedNextToken.strip("nd")
+				strippedNextToken = strippedNextToken.strip("rd")
+				strippedNextToken = strippedNextToken.strip("th")
+				if (strippedNextToken).isnumeric():
+					candidateDate+=(" "+tokens[monthIndex+2])
+
+				if monthIndex != 0:
+					strippedPriorToken = tokens[monthIndex-1].strip("st") #Anchor on the month, check to see if day or year is next
+					strippedPriorToken = strippedPriorToken.strip("nd")
+					strippedPriorToken = strippedPriorToken.strip("rd")
+					strippedPriorToken = strippedPriorToken.strip("th")
+					if (strippedPriorToken).isnumeric():
+						candidateDate+=(" "+tokens[monthIndex-1])
+					else:
+						pass
+
+			candidateMatch = list(datefinder.find_dates(candidateDate))			# Convert the candidate date into a date object
+
+			if len(candidateMatch) > 0:											# Convert to YYYY/MM/DD formatted string
+				for date in candidateMatch:
+					formattedMatches.append(date.strftime('%Y/%m/%d'))
+
+				dateToReturn = formattedMatches[0]
+
+		return dateToReturn
+
+
--- tests.py
+++ tests.py
@@ -3,10 +3,13 @@ import unittest
 from parser import logEntryParser
 
 class testInputHandling():
-	print("Tests not implemented")
+	print("testInputHandling not implemented")
 
 class testEntityExtraction(unittest.TestCase):
-		
+
+	def setUp(self):
+		self.namedEntites = ["HomerSimpson", "CarlCarlson", "MindySimmons"]
+		self.lp = logEntryParser()	
 
 	def testSuccessModes(self):
 		#Success Mode tests
@@ -16,45 +19,89 @@ class testEntityExtraction(unittest.TestCase):
 		hardTest = "On January 15th, 2021, @HomerSimpson was observed to perform to a satisfactory standard. \
 		This was evidenced by his successful completion of a routine safety check on Reactor 4. \
 		His actions show #competence #attentiontodetail #safetyconsciousness."
-		
+
+		succeedList = [easyTest, mediumTest1, mediumTest2, hardTest]
+
+		for test in succeedList:
+			self.assertEqual(self.lp.parseEntities(test, self.namedEntites), "HomerSimpson")
+
+
+	def testEntityMatching(self):
 		lowerCasingTest = "@homersimpson"
 		upperCasingTest = "@HOMERSIMPSON"
 		misspelledTest = "@HomerStimpson"
+		missingLetter = "@HmerSimpson"
+
+		entityMatchingTests = [lowerCasingTest, upperCasingTest, misspelledTest, missingLetter]
 		
+		for test in entityMatchingTests:
+			self.assertEqual(self.lp.parseEntities(test, self.namedEntites), "HomerSimpson")
+
+	def testFailureModes(self):
 		simpleFailureTest = "HomerSimpson"
 		multiEntityFailure = "Do not return HomerSimpson, return @BarneyGumble"
+		multiEntityChoosing = "Return @HomerSimpson, do not return @CarlCarlson"
 		distantStringFail = "Do not return @HomerThompson"
 		floatingAtFail = "Do not return the party @ noon"
 		noEntityTest = "Shouldnt return anything"
 
-		namedEntites = ["HomerSimpson", "CarlCarlson", "MindySimmons"]
-
-		succeedList = [easyTest, mediumTest1, mediumTest2, hardTest]
-		entityMatchingTests = [lowerCasingTest, upperCasingTest, misspelledTest]
-		failList = [simpleFailureTest, multiEntityFailure, distantStringFail, floatingAtFail, noEntityTest]
-
-		lp = logEntryParser()
-
-		for test in succeedList:
-			self.assertEqual(lp.parseEntities(test, namedEntites), "HomerSimpson")
-
-		for test in entityMatchingTests:
-			self.assertEqual(lp.parseEntities(test, namedEntites), "HomerSimpson")
+		failList = [simpleFailureTest, multiEntityFailure, multiEntityChoosing, distantStringFail, floatingAtFail, noEntityTest]
 
 		for test in failList:
-			self.assertNotEqual(lp.parseEntities(test, namedEntites), "HomerSimpson")
+			self.assertNotEqual(self.lp.parseEntities(test, self.namedEntites), "HomerSimpson")
 
-class testDateExtraction():
-	print("Tests not implemented")
+class testDateExtraction(unittest.TestCase):
+
+	def setUp(self):
+		self.lp_dates = logEntryParser()
+
+	def testSuccessModes(self):
+		americanDate = "02/28/2021"
+		americanDateShort = "02/28/21"
+		longDate = "February 28th 2021"
+		mediumDate = "28 Feb 21"
+		shortNoYear = "28 Feb"
+		longNoYear = "28 February"
+		longNoYear2 = "February 28th"
+		hardTest = "On February 28th 2021, @HomerSimpson was observed to perform to a satisfactory standard. \
+		This was evidenced by his successful completion of a routine safety check on Reactor 4. His actions \
+		show #competence #attentiontodetail #safetyconsciousness."
+
+		successList = [americanDate, americanDateShort, longDate, mediumDate, shortNoYear, longNoYear, longNoYear2, hardTest]
+
+		for test in successList:
+			self.assertEqual(self.lp_dates.parseDates(test, "2021"), "2021/02/28")
+
+
+	def testFailureModes(self):
+		restOfWorldObvious = "28/02/2021"
+		restOfWorldNotObvious = "04/02/2021"
+		restOfWorldObviousShort = "28/02/2021"
+		restOfWorldNotObvious = "04/02/21"
+
+		longDateNotExist = "February 34th 2021"
+		mediumDateNotExist = "34 Feb 21"
+		shortNoYearDateNotExist = "34 Feb"
+
+		#obviousWrongFormatList = [restOfWorldObvious, restOfWorldObviousShort]
+		notObviousWrongFormatList = [restOfWorldNotObvious, restOfWorldObviousShort]
+		notExistList = [longDateNotExist, mediumDateNotExist, shortNoYearDateNotExist]
+
+		#for test in obviousWrongFormatList:
+		#	self.assertNotEqual(self.lp_dates.parseDates(test, "2021"), "2021/02/28")
+
+		#for test in notObviousWrongFormatList:
+		#	self.assertNotEqual(self.lp_dates.parseDates(test, "2021"), "2021/04/02")
+
+		for test in notExistList:
+			self.assertNotEqual(self.lp_dates.parseDates(test, "2021"), "2021/02/34")
+
 
 class testTagExtraction():
-	print("Tests not implemented")
+	print("testTagExtraction not implemented")
 
 class testSentimentAnalysis():
-	print("Tests not implemented")
-
-
-#testEntityExtractor = testEntityExtraction()
+	print("testSentimentAnalysis not implemented")
 
 if __name__ == '__main__':
 	unittest.main()
