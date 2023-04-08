f89bd12519fa888b0160d83105be5fee053b8e73
Kent O'Sullivan <osullik@protonmail.com>
2023-04-08 08:41:57 -0400

    Naive Entity Extraction Implemented

--- parser.py
+++ parser.py
@@ -1,5 +1,12 @@
 
 import os
+import nltk
+from nltk.tree import Tree
+from nltk.metrics.distance  import edit_distance
+
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
 
 # Input Args:
 #	reporter	-	string	-	The person reporting the observation
@@ -18,5 +25,53 @@ import os
 
 class logEntryParser():
 
-	def __init__(reporter, dtg, logEntry):
-		pass
+	#def __init__():
+	#	pass
+
+	def parseEntities(self, logEntry, namedEntities):
+		'''
+		Extracts the target entities of the log entry
+
+		Input Args: 
+			logEntry - string - text of the log entry. 
+			namedEntites - list of strinfs - the list of valid entites in the database
+		Actions:
+			- Tokenizes the log with naive whitespace splitting
+			- Identifes entites with an @symbol prepended
+			- Matches them to entities in the database with string distance
+		Returns
+			- The target entity from the database
+		'''
+
+		targetEntity = None 											# Initialize Vars		
+		stringDistanceTolerance = 3										# User tunable Tolerance in string matching
+
+		naiveTokenization = logEntry.split(' ')							# Tokenize based on whitespace
+
+		for token in naiveTokenization:									# Loop through all tokens
+			if token[0] == "@":											# Identify those starting with @
+				candidateEntity = token[1:]								# Extract the likely entity 
+																			#TODO - Early Stopping
+
+				if candidateEntity in namedEntities:					# Check for exact entity matches
+					targetEntity = candidateEntity
+			
+				else:													# Check for close misses (spelling errors)
+					minDist = 1000
+					likelyCandidate = None
+					distList = []
+
+					for entity in namedEntities:
+						dist = edit_distance(candidateEntity.lower(), 
+							entity.lower())
+						distList.append((entity, dist))
+						
+						if dist < minDist:
+							minDist = dist 
+							likelyCandidate = entity
+
+					if minDist < stringDistanceTolerance:				# Return the closest string with < a threshold of error
+						targetEntity = likelyCandidate
+
+
+		return targetEntity
--- tests.py
+++ tests.py
@@ -5,8 +5,44 @@ from parser import logEntryParser
 class testInputHandling():
 	print("Tests not implemented")
 
-class testEntityExtraction():
-	print("Tests not implemented")
+class testEntityExtraction(unittest.TestCase):
+		
+
+	def testSuccessModes(self):
+		#Success Mode tests
+		easyTest = "@HomerSimpson"
+		mediumTest1 = "Should return @HomerSimpson"
+		mediumTest2 = "@HomerSimpson should be returned"
+		hardTest = "On January 15th, 2021, @HomerSimpson was observed to perform to a satisfactory standard. \
+		This was evidenced by his successful completion of a routine safety check on Reactor 4. \
+		His actions show #competence #attentiontodetail #safetyconsciousness."
+		
+		lowerCasingTest = "@homersimpson"
+		upperCasingTest = "@HOMERSIMPSON"
+		misspelledTest = "@HomerStimpson"
+		
+		simpleFailureTest = "HomerSimpson"
+		multiEntityFailure = "Do not return HomerSimpson, return @BarneyGumble"
+		distantStringFail = "Do not return @HomerThompson"
+		floatingAtFail = "Do not return the party @ noon"
+		noEntityTest = "Shouldnt return anything"
+
+		namedEntites = ["HomerSimpson", "CarlCarlson", "MindySimmons"]
+
+		succeedList = [easyTest, mediumTest1, mediumTest2, hardTest]
+		entityMatchingTests = [lowerCasingTest, upperCasingTest, misspelledTest]
+		failList = [simpleFailureTest, multiEntityFailure, distantStringFail, floatingAtFail, noEntityTest]
+
+		lp = logEntryParser()
+
+		for test in succeedList:
+			self.assertEqual(lp.parseEntities(test, namedEntites), "HomerSimpson")
+
+		for test in entityMatchingTests:
+			self.assertEqual(lp.parseEntities(test, namedEntites), "HomerSimpson")
+
+		for test in failList:
+			self.assertNotEqual(lp.parseEntities(test, namedEntites), "HomerSimpson")
 
 class testDateExtraction():
 	print("Tests not implemented")
@@ -16,3 +52,9 @@ class testTagExtraction():
 
 class testSentimentAnalysis():
 	print("Tests not implemented")
+
+
+#testEntityExtractor = testEntityExtraction()
+
+if __name__ == '__main__':
+	unittest.main()
